68ddf59afaefcffe9358c008886722f7aa7c4336
Joshua Dean <dea16006@byui.edu>
2023-05-12 21:50:12 -0600

    Added swagger api documentation

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+const swaggerAutogen = require('swagger-autogen')();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 app.use(bodyParser.json());
 app
-  .use("/", require("./routes"))
+  .use('/', require('./routes'))
+  .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
   .use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Origin', '*');
     next();
   })
-  .use("/", require("./routes"));
+  .use('/', require('./routes'));
 //app.use('/contacts', require('./routes/contact'));
 
 app.listen(process.env.port || 3000);
-console.log("Web Server is listening at port " + (process.env.port || 3000));
+console.log('Web Server is listening at port ' + (process.env.port || 3000));
